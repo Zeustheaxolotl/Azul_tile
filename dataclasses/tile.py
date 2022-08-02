e590b2d5f5f793fcedb96689c9451e74d53f6569
alyxbardoe <468ameliacheeta@gmail.com>
2022-08-02 10:54:44 -0400

    refactored the tile function to include a change location function and made the tilebag class

--- dataclasses/tile.py
+++ dataclasses/tile.py
@@ -1,16 +1,17 @@
-
 class Tile:
 
     def __init__(self, location, color):
-        self.color = color #what kind is the tile
-        self.location = location #where is the tile bag circle or playerboard
+        self.color = color
+        # what kind is the tile
+        self.location = location
+        # where is the tile bag circle or player board
 
     def __str__(self):
-        return self.color + ", " + self.location #say what color and where tile is
+        return self.color + ", " + self.location
+        # say what color and where tile is
 
-    def __repr__(self):
-        return str(self)
+    def show(self):
+        pass
 
-    def re_tile_bag(self):
-        if self.location == "tile bag":
-            tile_bag.append(self.color)
+    def reset_location(self, new_location):
+        self.location = new_location
---
+++ dataclasses/tilebag.py
@@ -0,0 +1,35 @@
+import random
+
+from tile import Tile
+
+
+class Tilebag:
+    def __init__(self):
+        self.tiles = []
+        self.colors = ['red', 'blue', 'yellow', 'black', 'teal']
+
+    def make_tiles(self):
+        for x in self.colors:
+            for y in range(20):
+                new_tile = Tile('tile bag', x)
+                self.tiles.append(new_tile)
+                #print(new_tile.__str__())
+
+    def draw_tiles(self, num, location):
+        drawn_tiles = []
+        for z in range(num):
+            i = random.randint(0, len(self.tiles))
+            self.tiles[i].reset_location(location)
+            drawn_tiles.append(self.tiles[i])
+            #print(self.tiles[i].__str__())
+            self.tiles.pop(i)
+            return drawn_tiles
+
+
+
+
+#new_tilebag = Tilebag()
+#new_tilebag.make_tiles()
+#new_tilebag.draw_tiles(4, "tile circle")
+
+
