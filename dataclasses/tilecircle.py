b5876e2ef32faeacc11b9b78237e39c239ca4ea4
mbardoe <mbardoe@gmail.com>
2022-08-02 11:42:46 -0400

    Creating tilecircle to be shown. Added a runnable component to tilecircle to test seeing the tiles and tilecircle.

--- dataclasses/tilecircle.py
+++ dataclasses/tilecircle.py
@@ -1,32 +1,59 @@
 import random
-from tilebag import TileBag
+import pygame, sys
+from tilebag import Tilebag
+from pygame.locals import *
+
 
 class TileCircle:
-    def __init__(self, tile_bag: TileBag):
-        self.tile_bag=tile_bag
+    def __init__(self, tile_bag: Tilebag):
+        self.tile_bag = tile_bag
         self.tiles = []
+        self.image = pygame.image.load('../img/tile_circle_200x200.png')
 
+    def draw_tiles_from_bag(self, num=4):
+        self.tiles.append(self.tile_bag.draw_tiles(num, "tile_circle"))
 
-    def calc_tiles(self):
-        #random.shuffle(self.tile_bag) # tile_bag does this automatically
-        #for x in range(4):
-        self.tiles.append(self.tile_bag.drawTiles(4))
-            #del self.tile_bag[-1] # tile bag does this
-       #rnum = random.sample(range(0, len(tile_bag)), 4)
-      # for z in rnum:
-     #       rtile = tile_bag[z]
-    #        rnums.append(z)
-   #         del tile_bag[z]
-  #          self.tiles.append(rtile)
-#
-#           print(self.tiles)
-    def show(self):
-        for x in range(len(self.tiles)):
-            print(self.tiles[x])
+    def show(self, screen, x, y):
+        screen.blit(self.image, (x, y))
+        if len(self.tiles) == 1:
+            screen.blit.self.tiles[1].show(screen, x + 100, y + 100)
+            print(self.tiles[1])
+        if len(self.tiles) == 2:
+            screen.blit.self.tiles[1].show(screen, x + 40, y + 100)
+            print(self.tiles[1])
+            screen.blit.self.tiles[2].show(screen, x + 140, y + 100)
+            print(self.tiles[2])
+        if len(self.tiles) == 3:
+            screen.blit.self.tiles[1].show(screen, x + 100, y + 40)
+            print(self.tiles[1])
+            screen.blit.self.tiles[2].show(screen, x + 40, y + 120)
+            print(self.tiles[2])
+            screen.blit.self.tiles[3].show(screen, x + 140, y + 120)
+            print(self.tiles[3])
+        if len(self.tiles) == 4:
+            screen.blit.self.tiles[1].show(screen, x + 40, y + 40)
+            print(self.tiles[1])
+            screen.blit.self.tiles[2].show(screen, x + 40, y + 120)
+            print(self.tiles[2])
+            screen.blit.self.tiles[3].show(screen, x + 40, y + 120)
+            print(self.tiles[3])
+            screen.blit.self.tiles[4].show(screen, x + 140, y + 120)
+            print(self.tiles[4])
+
 
 if __name__ == "__main__":
-    tile_bag=TileBag()
-    tile_bag.initialize()
-    tile_circle=TileCircle(tile_bag)
-    tile_circle.calc_tiles()
-    tile_circle.show()
+    tile_bag = Tilebag()
+    tile_bag.make_tiles()
+    tile_circle = TileCircle(tile_bag)
+    tile_circle.draw_tiles_from_bag()
+
+    pygame.init()
+    display = pygame.display.set_mode((400, 300))
+    pygame.display.set_caption('Hello World!')
+    while True:  # main game loop
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        tile_circle.show(display, 100, 100)
+        pygame.display.update()
