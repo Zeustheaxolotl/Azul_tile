f9890ad32a45758ee55d333a5b7faca661c4e58d
mbardoe <mbardoe@gmail.com>
2022-08-03 10:28:27 -0400

    Changed the screen class to an abstract class.

--- dataclasses/screens/screen.py
+++ dataclasses/screens/screen.py
@@ -1,19 +1,23 @@
-import pygame
-import sys
+from abc import ABC, abstractmethod
+
+from dataclasses.game import Game
 
 
-class Screen:
+class Screen(ABC):
 
-    def __init__(self, game):
+    def __init__(self, game: Game):
         self.display = game.display
         self.screen_dim = game.screen_dim
         self.game = game
 
+    @abstractmethod
     def show(self):
         pass
 
+    @abstractmethod
     def listen(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+        pass
+    # for event in pygame.event.get():
+    #     if event.type == pygame.QUIT:
+    #         pygame.quit()
+    #         sys.exit()
