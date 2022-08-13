227ad8c6059632982222e17bf840056b20d6eb9b
mbardoe <mbardoe@gmail.com>
2022-08-12 20:35:42 -0400

    refactoring tile circlee

--- dataclasses/tilecircle.py
+++ dataclasses/tilecircle.py
@@ -54,6 +54,31 @@ class TileCircle:
     def collide_tile_circle(self, x, y):
         return self.rect.collidepoint(x, y)
 
+    def get_clicked_tiles(self, event):
+        """
+
+        :param event: A pygame event
+
+        :return: Tuple with tiles matching the clicked tiles and the rest of the tiles
+        """
+        if event.type == MOUSEBUTTONDOWN:
+            print(event.pos)
+            x, y = event.pos
+
+            if self.collide_tile_circle(x, y):  # hit the circle
+                hit_tile = self.collide_tiles(x, y)  # Was a tile hit?
+                if hit_tile:
+                    matching_tiles = []
+                    unmatching_tiles = []
+                    for tile in self.tiles:
+                        if tile.color == hit_tile.color:
+                            matching_tiles.append(tile)
+                        else:
+                            unmatching_tiles.append(tile)
+                    self.tiles = []
+                    return (matching_tiles, unmatching_tiles)
+        return None
+
 
 class Test_Tile_Circle_Screen(Screen):
 
@@ -63,6 +88,7 @@ class Test_Tile_Circle_Screen(Screen):
         # self.game = game
         self.tile_circle = tile_circle
         self.tiles = []
+        self.center_tiles = []
 
     def show(self):
         # print("here")
@@ -74,31 +100,47 @@ class Test_Tile_Circle_Screen(Screen):
             tile.show(self.display, x + offset_x, y + offset_y)
             offset_x += 45
 
+        x = 100
+        y = 100
+        offset_x = 0
+        offset_y = 0
+        for tile in self.center_tiles:
+            tile.show(self.display, x + offset_x, y + offset_y)
+            offset_x += 45
+            if offset_x > 150:
+                offset_y += 45
+                offset_x = 0
+
     def listen(self):
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
 
-            if event.type == MOUSEBUTTONDOWN:
-                print(event.pos)
-                x, y = event.pos
-                hit_circle = self.tile_circle.collide_tile_circle(x, y)
-                hit_tile = self.tile_circle.collide_tiles(x, y)
-                if hit_circle and len(self.tiles) > 0:
-                    for tile in self.tiles:
-                        tile.location = "tile circle"
-                        self.tile_circle.tiles.append(tile)
-                    self.tiles.clear()
+            tiles = self.tile_circle.get_clicked_tiles(event)
+            if tiles:
+                self.tiles += tiles[0]
+                self.center_tiles += tiles[1]
 
-                if hit_tile and len(self.tiles) == 0:
-                    print(hit_tile)
-                    for tile in self.tile_circle.tiles:
-                        if tile.color == hit_tile.color:
-                            tile.location = "chosen"
-                            self.tiles.append(tile)
-                    for tile in self.tiles:
-                        self.tile_circle.tiles.remove(tile)
+            # if event.type == MOUSEBUTTONDOWN:
+            #     print(event.pos)
+            #     x, y = event.pos
+            #     hit_circle = self.tile_circle.collide_tile_circle(x, y)
+            #     hit_tile = self.tile_circle.collide_tiles(x, y)
+            #     if hit_circle and len(self.tiles) > 0:
+            #         for tile in self.tiles:
+            #             tile.location = "tile circle"
+            #             self.tile_circle.tiles.append(tile)
+            #         self.tiles.clear()
+            #
+            #     if hit_tile and len(self.tiles) == 0:
+            #         print(hit_tile)
+            #         for tile in self.tile_circle.tiles:
+            #             if tile.color == hit_tile.color:
+            #                 tile.location = "chosen"
+            #                 self.tiles.append(tile)
+            #         for tile in self.tiles:
+            #             self.tile_circle.tiles.remove(tile)
 
 
 
